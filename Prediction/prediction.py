a65a0882af4db201c041d8d65c600ffe78155c60
Brlsadiksha <113443083+Brlsadiksha@users.noreply.github.com>
2023-05-31 16:20:26 +0200

    Updated files to DKK per kwh

--- Prediction/prediction.py
+++ Prediction/prediction.py
@@ -8,7 +8,8 @@ app = FastAPI()
 
 @app.get("/predict/{num_pred}")
 def results(num_pred):
-    model_uri = "models:/model_es_pred/latest"
+    model_uri ='models:/model_es_pred/latest'
     loaded_model = mlflow.pyfunc.load_model(model_uri)
     pred = loaded_model.predict(int(num_pred))
     return pred.pd_dataframe().to_dict() #change according to datatype needed to streamlit
+
